a34e883cad1b0e92d0c4c7acb44ac2b2d3e4039b
goronke <worsacam@gmail.com>
2025-05-12 20:36:15 +0300

    Две апишки готово

--- index.js
+++ index.js
@@ -61,6 +61,57 @@ app.get('/accountant/payments', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+app.get('/accountant/salary', async (req, res) => {
+  const { startDate, endDate } = req.query;
+  if (!startDate || !endDate) {
+    return res.status(400).json({ error: 'startDate and endDate are required' });
+  }
+  try {
+    const query = `
+ SELECT
+  c.name AS "Тренер",
+  cs.rank AS "Квалификация",
+  cs.class_cost AS "Стоимость одного занятия",
+  COUNT(cl.id) AS "Количество занятий за выбранный период",
+  COUNT(cl.id) * cs.class_cost AS "К выплате на руки",
+  ROUND(COUNT(cl.id) * cs.class_cost * 1.149425, 2) AS "К выплате в ГРОСС"
+FROM public.classes cl
+JOIN public."groups" g ON cl.group_id = g.id
+JOIN public.couches c ON g.couch_id = c.id
+JOIN public.coaches_salary cs ON c.salary_id = cs.id
+  AND cl.date_time BETWEEN $1 AND $2
+GROUP BY c.id, c.name, cs.rank, cs.class_cost
+ORDER BY "К выплате в ГРОСС" DESC;
+    `;
+    const { rows } = await pool.query(query, [startDate, endDate]);
+
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Зарплаты');
+    worksheet.columns = [
+      { header: 'Тренер', key: 'Тренер', width: 20 },
+      { header: 'Квалификация', key: 'Квалификация', width: 20 },
+      { header: 'Стоимость одного занятия', key: 'Стоимость одного занятия', width: 20 },
+      { header: 'Количество занятий за выбранный период', key: 'Количество занятий за выбранный период', width: 25 },
+      { header: 'К выплате на руки', key: 'К выплате на руки', width: 20 },
+      { header: 'К выплате в ГРОСС', key: 'К выплате в ГРОСС', width: 20 },
+    ];
+    rows.forEach(row => worksheet.addRow(row));
+
+    const buffer = await workbook.xlsx.writeBuffer();
+    const base64 = Buffer.from(buffer).toString('base64');
+    const fileName = `Тренерские зарплаты ${startDate} - ${endDate}.xlsx`;
+
+    res.status(200).json({
+      fileName,
+      fileData: base64
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
